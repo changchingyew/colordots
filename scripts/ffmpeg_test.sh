e8b2e2e82ca832a9f115f55d8c824e34ab696102
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-12-01 17:13:07 +0000

    wip test with rtp

--- scripts/ffmpeg_test.sh
+++ scripts/ffmpeg_test.sh
@@ -5,8 +5,18 @@ SCRIPT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]:-$0}")" >/dev/null 2>&1 ; pwd -P 
 
 DEBUG="-loglevel verbose"
 
-OUTPUT="-frames:v 10 -vf crop=500:500:0:0 -y crop_0.yuv\
-        -frames:v 10 -vf crop=500:500:500:0 -y crop_1.yuv"
+# for i in `seq 1 20`;
+# do
+# 	./test_driver.pl -s beh_avce_nummbperslice -p u18.04_kmb_64 --gold no -i $i
+# done
+
+
 # OUTPUT="-frames:v 10 -y out.yuv"
+# OUTPUT="-frames:v 10 -vf crop=500:500:0:0 -y crop_0.yuv\
+#         -frames:v 10 -vf crop=500:500:500:0 -y crop_1.yuv"
+# OUTPUT="-frames:v 10 -vf crop=500:500:0:0 -f rtp rtp://127.0.0.1:2001\
+#         -frames:v 10 -vf crop=500:500:500:0 -f rtp rtp://127.0.0.1:2002"
+OUTPUT="-vf crop=500:500:0:0 -f rtp rtp://127.0.0.1:2001\
+        -vf crop=500:500:500:0 -f rtp rtp://127.0.0.1:2002"
 
 ${FFMPEG_PATH}/ffmpeg_g -c:v ${CODEC} ${DEBUG} -i ${MEDIA} ${OUTPUT}
